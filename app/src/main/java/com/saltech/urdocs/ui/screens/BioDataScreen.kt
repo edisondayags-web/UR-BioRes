5e8aeef2ab3ea6788dcd1fc7eae80c0f6c1c081e
edisondayags-web <edisondayags@gmail.com>
2026-07-16 14:23:34 +0800

    Update BioDataScreen.kt

--- app/src/main/java/com/saltech/urdocs/ui/screens/BioDataScreen.kt
+++ app/src/main/java/com/saltech/urdocs/ui/screens/BioDataScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
