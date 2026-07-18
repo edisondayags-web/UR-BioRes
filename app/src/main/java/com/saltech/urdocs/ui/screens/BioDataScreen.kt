c7bcc0e6e83ab3b881335cf2ea1ba6653974c0e7
edisondayags-web <edisondayags@gmail.com>
2026-07-18 23:58:47 +0800

    Update BioDataScreen.kt

--- app/src/main/java/com/saltech/urdocs/ui/screens/BioDataScreen.kt
+++ app/src/main/java/com/saltech/urdocs/ui/screens/BioDataScreen.kt
@@ -138,7 +138,7 @@ fun BioDataScreen(
             Image(
         painter = painterResource(id = R.drawable.ic_launcher_bg),
         contentDescription = null,
-        modifier = Modifier.fillMaxSize().scale(1.3f),
+        modifier = Modifier.fillMaxSize().scale(1.1f),
         contentScale = ContentScale.Crop
     )
         val fitScale = minOf(maxWidth / paperWidthDp, maxHeight / paperHeightDp)
