2f510353cee736bbecc344e563e1b7aedb6a7aab
edisondayags-web <edisondayags@gmail.com>
2026-07-19 06:38:29 +0800

    Update TraditionalResumeScreen.kt

--- app/src/main/java/com/saltech/urdocs/ui/screens/TraditionalResumeScreen.kt
+++ app/src/main/java/com/saltech/urdocs/ui/screens/TraditionalResumeScreen.kt
@@ -134,6 +134,12 @@ fun TraditionalResumeScreen(
         modifier = Modifier.fillMaxSize().background(Color(0xFFCFCFCF))
     ) {
         val fitScale = minOf(maxWidth / paperWidthDp, maxHeight / paperHeightDp)
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_bg),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize().alpha(1.5f),
+            contentScale = ContentScale.Crop
+        )
         var scale by remember { mutableStateOf(fitScale) }
 
         Box(
@@ -168,13 +174,6 @@ fun TraditionalResumeScreen(
                 .background(Color.White)
                 .padding(28.dp)
         ) {
-
-      Image(  
-          painter = painterResource(id = R.drawable.ic_launcher_bg),  
-    contentDescription = null,  
-    modifier = Modifier.fillMaxSize().scale(1.1f).alpha(0.08f),
-    contentScale = ContentScale.Crop  
-)
             Column(modifier = Modifier.fillMaxSize()) {
 
                 Text(
