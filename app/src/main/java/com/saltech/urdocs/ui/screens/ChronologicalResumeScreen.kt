9c68e376d56387fabaca8e04ee381be505e1556e
edisondayags-web <edisondayags@gmail.com>
2026-07-19 00:54:35 +0800

    Update ChronologicalResumeScreen.kt

--- app/src/main/java/com/saltech/urdocs/ui/screens/ChronologicalResumeScreen.kt
+++ app/src/main/java/com/saltech/urdocs/ui/screens/ChronologicalResumeScreen.kt
@@ -138,7 +138,7 @@ fun ChronologicalResumeScreen() {
             Image(  
                painter = painterResource(id = R.drawable.ic_launcher_bg),  
                 contentDescription = null,  
-                 modifier = Modifier.fillMaxSize().scale(1.1f),  
+                 modifier = Modifier.fillMaxSize().scale(1.1f).alpha(0.08f),
                   contentScale = ContentScale.Crop  
         )
             Column(modifier = Modifier.fillMaxSize()) {
