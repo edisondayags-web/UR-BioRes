95d9a78bcf854acafcb1946887c701393ded6339
edisondayags-web <edisondayags@gmail.com>
2026-07-19 13:11:24 +0800

    Naneto

--- app/src/main/java/com/saltech/urdocs/navigation/Screen.kt
+++ app/src/main/java/com/saltech/urdocs/navigation/Screen.kt
@@ -8,6 +8,11 @@ sealed class Screen(val route: String) {
     object BioData : Screen("biodata")
     object GovtForms : Screen("govt_forms")
     object Letters : Screen("letters")
+    object Settings : Screen("settings")
+    object PrivacyPolicy : Screen("privacy_policy")
+    object TermsConditions : Screen("terms_conditions")
+    object DataPermissions : Screen("data_permissions")
+    object AboutDeveloper : Screen("about_developer")
     object SelfieCapture : Screen("selfie_capture/{returnTo}") {
         fun createRoute(returnTo: String) = "selfie_capture/$returnTo"
     }
