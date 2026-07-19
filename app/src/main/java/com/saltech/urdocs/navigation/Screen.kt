fa918142fc25b987c52f2e3597148f33a2760882
edisondayags-web <edisondayags@gmail.com>
2026-07-19 16:58:21 +0800

    Bat binasa mo, baba ka no

--- app/src/main/java/com/saltech/urdocs/navigation/Screen.kt
+++ app/src/main/java/com/saltech/urdocs/navigation/Screen.kt
@@ -14,6 +14,9 @@ sealed class Screen(val route: String) {
     object DataPermissions : Screen("data_permissions")
     object AboutDeveloper : Screen("about_developer")
     object SelfieCapture : Screen("selfie_capture/{returnTo}") {
-        fun createRoute(returnTo: String) = "selfie_capture/$returnTo"
+      fun createRoute(returnTo: String) = "selfie_capture/$returnTo"
+    }
+    object LetterAssistant : Screen("letter_assistant/{letterType}") {
+      fun createRoute(letterType: String) = "letter_assistant/$letterType"
     }
 }
