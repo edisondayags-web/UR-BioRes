07350a72f7bd136af6024dac5f276169273bfc22
edisondayags-web <edisondayags@gmail.com>
2026-07-19 09:39:56 +0800

    Update TraditionalResumeScreen.kt

--- app/src/main/java/com/saltech/urdocs/ui/screens/TraditionalResumeScreen.kt
+++ app/src/main/java/com/saltech/urdocs/ui/screens/TraditionalResumeScreen.kt
@@ -138,7 +138,7 @@ fun TraditionalResumeScreen(
         Image(
             painter = painterResource(id = R.drawable.ic_launcher_bg),
             contentDescription = null,
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxSize().scale(1.1f),
             contentScale = ContentScale.Crop
         )
         var scale by remember { mutableStateOf(fitScale) }
