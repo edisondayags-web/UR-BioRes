6af1826978d76bbf83f68ae64f6f5252f80d8322
edisondayags-web <edisondayags@gmail.com>
2026-07-19 08:32:03 +0800

    Update ResumeChoiceScreen.kt

--- app/src/main/java/com/saltech/urdocs/ui/screens/ResumeChoiceScreen.kt
+++ app/src/main/java/com/saltech/urdocs/ui/screens/ResumeChoiceScreen.kt
@@ -1,6 +1,7 @@
 package com.saltech.urdocs.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
