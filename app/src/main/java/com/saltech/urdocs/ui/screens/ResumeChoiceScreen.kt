9c2bbd36891a4f6a4ed91291d8e9965e15cecc69
edisondayags-web <edisondayags@gmail.com>
2026-07-19 08:08:21 +0800

    Update ResumeChoiceScreen.kt

--- app/src/main/java/com/saltech/urdocs/ui/screens/ResumeChoiceScreen.kt
+++ app/src/main/java/com/saltech/urdocs/ui/screens/ResumeChoiceScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
