4ec86b76a2f9d62fee680dbcca5efe618e512d3e
Edison Dayaguit <your-github-email@example.com>
2026-07-26 21:44:50 +0800

    Fix missing imports in HomeScreen.kt

--- app/src/main/java/com/saltech/urdocs/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/saltech/urdocs/ui/screens/HomeScreen.kt
@@ -15,6 +15,9 @@ import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.foundation.border
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
