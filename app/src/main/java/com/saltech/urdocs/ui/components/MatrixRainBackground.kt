ed9944bba3ab18a0d8925e1021351c49b9fa1e26
edisondayags-web <edisondayags@gmail.com>
2026-07-26 16:05:30 +0800

    Update MatrixRainBackground.kt

--- app/src/main/java/com/saltech/urdocs/ui/components/MatrixRainBackground.kt
+++ app/src/main/java/com/saltech/urdocs/ui/components/MatrixRainBackground.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.core.*
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.nativeCanvas
