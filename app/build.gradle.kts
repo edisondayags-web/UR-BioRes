831ef3d21f69956c93faa597c1b95df4dc5e90cb
Edison Dayaguit <your-github-email@example.com>
2026-07-29 11:53:21 +0800

    Upgrade compose-bom to 2024.12.01 for graphicsLayer support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,7 +79,7 @@ defaultConfig {
 dependencies {
     // Compose
     implementation("androidx.browser:browser:1.8.0")
-    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
