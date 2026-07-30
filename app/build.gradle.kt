a7b5cca14bd23aa7471b42878f27a820bb65a1c5
edisondayags-web <edisondayags@gmail.com>
2026-07-31 00:27:30 +0800

    Update build.gradle.kt

--- app/build.gradle.kt
+++ app/build.gradle.kt
@@ -59,7 +59,7 @@ android {
 
 dependencies {
     // Compose
-
+    implementation("androidx.compose.foundation:foundation-layout-android")
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
