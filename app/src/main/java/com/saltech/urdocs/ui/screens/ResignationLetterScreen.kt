197bd6f2a1dcc80fa5885d048d5e4ee63de24c68
edisondayags-web <edisondayags@gmail.com>
2026-07-31 09:48:07 +0800

    Update ResignationLetterScreen.kt

--- app/src/main/java/com/saltech/urdocs/ui/screens/ResignationLetterScreen.kt
+++ app/src/main/java/com/saltech/urdocs/ui/screens/ResignationLetterScreen.kt
@@ -135,7 +135,7 @@ fun ResignationLetterScreen() {
                         modifier = Modifier.fillMaxWidth()
                     )
 
-                    Spacer(Modifier.weight(0.5f))
+                    Spacer(Modifier.weight(0.7f))
 
 // Date
 RLField("Date", data.date, fieldWidth = 130.dp) { data = data.copy(date = it) }
@@ -209,7 +209,7 @@ FlowRow(modifier = Modifier.fillMaxWidth()) {
 
                   Spacer(Modifier.padding(start = 60.dp)) // hindi ito gagana mag-isa, tignan sa baba
 
-Spacer(Modifier.weight(0.5f))
+Spacer(Modifier.weight(0.08f))
 
 Box(modifier = Modifier.padding(start = 60.dp)) {
     RLPlainText("Yours sincerely,")
