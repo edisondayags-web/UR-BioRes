9b8e7f6603edfa97f801c30646e24801bbcc82f9
edisondayags-web <edisondayags@gmail.com>
2026-08-06 09:18:02 +0800

    ♠️♣️

--- app/src/main/java/com/saltech/urdocs/ui/screens/HybridResumeScreen.kt
+++ app/src/main/java/com/saltech/urdocs/ui/screens/HybridResumeScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.geometry.Offset
