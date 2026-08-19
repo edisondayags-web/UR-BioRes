15afb0004604c3f8a9e400763e9f0999fd12cb04
edisondayags-web <edisondayags@gmail.com>
2026-08-19 14:39:12 +0800

    Update BiodataTemplate11_PixelPerfect.kt

--- app/src/main/java/com/saltech/urdocs/ui/templates/BiodataTemplate11_PixelPerfect.kt
+++ app/src/main/java/com/saltech/urdocs/ui/templates/BiodataTemplate11_PixelPerfect.kt
@@ -175,7 +175,7 @@ fun BiodataTemplate11_PixelPerfect(
 ) {
     val accent1 = Color(0xFF6A9B7A)
     val accent2 = Color(0xFF8BC34A)
-    val isDark = false
+    val isDark = true
     val bgColor = if(isDark) Color(0xFF0A0A0A) else Color.White
     val graphicsLayer = androidx.compose.ui.graphics.rememberGraphicsLayer()
     
