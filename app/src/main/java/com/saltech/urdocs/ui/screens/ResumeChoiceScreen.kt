bd44bbec5c93322ae25c9a42ce1afafb4072bd5f
edisondayags-web <edisondayags@gmail.com>
2026-09-02 08:47:30 +0800

    Update ResumeChoiceScreen.kt

--- app/src/main/java/com/saltech/urdocs/ui/screens/ResumeChoiceScreen.kt
+++ app/src/main/java/com/saltech/urdocs/ui/screens/ResumeChoiceScreen.kt
@@ -76,10 +76,10 @@ fun ResumeChoiceScreen(onChoose: (String) -> Unit, onBack: () -> Unit = {}) {
             }
             Spacer(Modifier.height(10.dp))
             Row {
-                Text("Pili ka ng ", color = RGray, fontSize = 14.sp)
-                Text("gusto", color = Color.White, fontSize = 14.sp)
-                Text(" mong ", color = RGray, fontSize = 14.sp)
-                Text("format", color = Color.White, fontSize = 14.sp)
+                Text("Tap ", color = RGray, fontSize = 14.sp)
+                Text("ka ", color = Color.White, fontSize = 14.sp)
+                Text(" lang ", color = RGray, fontSize = 14.sp)
+                Text("dyan", color = Color.White, fontSize = 14.sp)
                 Text(" Luv.", color = RGray, fontSize = 14.sp)
             }
 
